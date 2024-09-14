1a8a7406b7cedda3f3317bae67598ea289e1b342
siranjivikarnan <siranjivikarnan@gmail.com>
2024-09-14 14:47:29 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function btn(){
+ const btn = document.querySelector ('.btn');
+ const imglist = ['CSS/Images/elx2Pal-one-piece-high-definition-wallpaper.jpg','CSS/Images/wp14319175-no-longer-allowed-in-another-world-wallpapers.jpg','CSS/Images/wp11248636-vinland-saga-manga-wallpapers.jpg']
+ console.log('clicked');
+}
